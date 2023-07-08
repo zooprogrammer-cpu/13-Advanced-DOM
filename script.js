885bc8bf8a7e8409eef5877410b65cb92c32261c
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-07-08 15:40:26 -0500

    selecting creating and deleting elements

--- script.js
+++ script.js
@@ -35,3 +35,45 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+//can use getElementsByTageName to delete.
+//cannot delete with querySelector node list
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+//Creating and inserting elements
+//.insertAdjacentHTML - quick and most used. check bankist app
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML = 'Cookie here. <button class="btn btn--close--cookie">Got it!</button>';
+// cannot have message element at multiples places. 
+// So the append moves the element as the last child. 
+// header.prepend(message);
+header.append(message); 
+// header.append(message.cloneNode(true));// this create two
+
+// header.before(message);
+// header.after(message);
+
+// Delete elements
+document.querySelector('.btn--close--cookie').addEventListener('click', function(){
+  message.remove(); 
+  // older method by dom traversing
+  // message.parentElement.removeChild(message); 
+});
+
+
+
