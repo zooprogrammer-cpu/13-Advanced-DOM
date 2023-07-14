000f4ed5f5a113557edddc036fdccee593dee95e
zooprogrammer-cpu <60715599+zooprogrammer-cpu@users.noreply.github.com>
2023-07-14 09:54:34 -0500

    Attributes - src, class, alt, id

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
 const openModal = function (e) {
-  e.preventDefault(); 
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -20,8 +20,8 @@ const closeModal = function () {
 };
 
 // Open modal for button clicks
-btnsOpenModal.forEach((btn)=>{
-  btn.addEventListener('click', openModal);  
+btnsOpenModal.forEach((btn) => {
+  btn.addEventListener('click', openModal);
 })
 
 // for (let i = 0; i < btnsOpenModal.length; i++)
@@ -62,18 +62,58 @@ message.innerHTML = 'Cookie here. <button class="btn btn--close--cookie">Got it!
 // cannot have message element at multiples places. 
 // So the append moves the element as the last child. 
 // header.prepend(message);
-header.append(message); 
+header.append(message);
 // header.append(message.cloneNode(true));// this create two
 
 // header.before(message);
 // header.after(message);
 
 // Delete elements
-document.querySelector('.btn--close--cookie').addEventListener('click', function(){
-  message.remove(); 
+document.querySelector('.btn--close--cookie').addEventListener('click', function () {
+  message.remove();
   // older method by dom traversing
   // message.parentElement.removeChild(message); 
 });
 
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+// Attributes - src, class, alt, id
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.className);
+logo.alt = 'Beautiful';
+
+// Non- standard attribute
+console.log(logo.designer); // prints undefined
+console.log('desginer is ', logo.getAttribute('designer')); //prints designer is ash
+logo.setAttribute('company', 'Bankist'); // creates a new attribute called company="Bankist"
+
+console.log('logo.src is:', logo.src); // this is the absolute url 
+console.log('logo.getAttribute is:', logo.getAttribute('src')); // this is the relative url 
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // prints http://127.0.0.1:8080/#
+console.log(link.getAttribute('href')); // prints #
 
 
+//Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
